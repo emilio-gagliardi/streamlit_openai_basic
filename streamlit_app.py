3c494b1e8d64ece99c7096c1969ca5d7a2976bdb
Emilio Gagliardi <emilio.gagliardi@gmail.com>
2023-06-03 22:52:36 -0600

    fixed api access

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from langchain.llms import OpenAI
 
 st.title('My First LangChain app')
-openai_api_key = st.secrets["openai_api"]["key"]
+openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
 
 def generate_response(input_text):
@@ -13,5 +13,7 @@ def generate_response(input_text):
 with st.form('my_form'):
     text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
     submitted = st.form_submit_button('Submit')
+    if not openai_api_key.startswith('sk-'):
+        st.warning('Please enter your OpenAI API key!', icon='⚠')
     if submitted and openai_api_key.startswith('sk-'):
         generate_response(text)
