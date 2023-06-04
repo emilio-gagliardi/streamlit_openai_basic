282c805db8049762789bab2b0d11ab2123002716
Emilio Gagliardi <emilio.gagliardi@gmail.com>
2023-06-03 21:15:06 -0600

    added langchain functions

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,17 @@
 import streamlit as st
+from langchain.llms import OpenAI
 
-st.title('🎈 App Name')
+st.title('My First LangChain app')
+openai_api_key = "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"
 
-st.write('Hello world!')
+
+def generate_response(input_text):
+    llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+    st.info(llm(input_text))
+
+
+with st.form('my_form'):
+    text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+    submitted = st.form_submit_button('Submit')
+    if submitted and openai_api_key.startswith('sk-'):
+        generate_response(text)
